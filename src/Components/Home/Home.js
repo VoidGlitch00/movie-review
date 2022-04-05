314337a093855d00d32c864fddb3bedb89861871
VoidGlitch00 <shawonsk09@gmail.com>
2022-04-05 23:59:53 +0600

    done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,21 +1,25 @@
 import React from 'react';
+import Reviewer from '../Reviewer/Reviewer';
+import Reviews from '../Reviews/Reviews';
+import CustomHook from '../../CustomHooks/CustomHooks';
 
 const Home = () => {
     return (
         <div>
             <div className="container mt-5 mb-5 d-flex justify-content-between">
-            <div className="d-flex align-items-center">
+                <div className="d-flex align-items-center">
+                    <div>
+                        <h2>One of the <span className="fw-bold" style={{ color: '#8D3DAF' }}>Best Movies</span> Till Date</h2>
+                        <p>What could possibly make a movie Top Rated? Viewers review and rating could be a milestone for a movie to be the greatest of all time! Let's explore the reviews of "The Godfather", one of the greatest of all time...</p>
+                    </div>
+                </div>
                 <div>
-                    <h2>One of the <span className="fw-bold" style={{ color: '#8D3DAF' }}>Best Movies</span> Till Date</h2>
-                    <p>What could possibly make a movie Top Rated? Viewers review and rating could be a milestone for a movie to be the greatest of all time! Let's explore the reviews of "The Godfather", one of the greatest of all time...</p>
+                    <img className="w-75" src="https://cpb-us-w2.wpmucdn.com/blogs.iu.edu/dist/f/65/files/2019/04/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_-1odtbn5.jpg"></img>
                 </div>
             </div>
-            <div>
-                <img className="w-75" src="https://cpb-us-w2.wpmucdn.com/blogs.iu.edu/dist/f/65/files/2019/04/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_-1odtbn5.jpg"></img>
-            </div>
-        </div>
-        <h2>Movie Freak Reviews!(3)</h2>
-            
+            <h2>Movie Freak Reviews!(3)</h2>
+
+
         </div>
     );
 };
--- src/Components/Reviewer/Reviewer.js
+++ src/Components/Reviewer/Reviewer.js
@@ -4,7 +4,7 @@ const Reviewer = (props) => {
     const { name, review, ratings } = props.review;
     return (
         <div className="container mx-auto justify-content-center mt-5 d-flex col-xl-4 col-sm-12">
-            <div className="card" style={{width: '15rem'}}>
+            <div className="card" style={{width: '25rem'}}>
                 <div className="card-body">
                     <h3>{name}</h3>
                     <p className="card-text">{review}
